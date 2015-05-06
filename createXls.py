e794bbfb69a265b5b596144d00d4750f6cc2604f
Tyler <givemealetter@gmail.com>
2015-05-06 16:04:03 +0800

    hotfix ent to env

--- createXls.py
+++ createXls.py
@@ -1,5 +1,5 @@
 #!coding: utf-8
-#!/bin/ent python
+#!/bin/env python
 import xlrd 
 import xlwt
 
--- readXls.py
+++ readXls.py
@@ -1,5 +1,5 @@
 #!coding: utf-8
-#!/bin/ent python
+#!/bin/env python
 import types
 import xlrd 
 import xlwt
